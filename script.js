85c8a049981f61682bcbf1edc345b72790da9225
Isabela da Costa <91699692+isabeladcp@users.noreply.github.com>
2021-10-01 14:54:06 -0300

    Adicionei os arquivos da imersão.dev

---
+++ script.js
@@ -0,0 +1,12 @@
+var dark = true;
+
+function mudaTema(){
+  document.body.classList.toggle('dark')
+  if (dark) {
+		dark = false;
+		document.getElementById("tema").innerHTML = "🌙";
+	} else {
+		dark = true;
+		document.getElementById("tema").innerHTML = "☀️";
+	}
+}
